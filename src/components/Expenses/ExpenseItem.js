911261870f71661be2a4cc0cb38b3d493369c42f
gingercook <vahitaras@gmail.com>
2021-11-21 00:22:47 -0500

    select tag has an event handler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,24 @@
 // You never need to import React module but in old
 // project they used to import.
 // import React from 'react';
-import React, { useState } from 'react';
+import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
+// This component is a stateless component. It is out there to just
+// show data.
+// If there was a state (useState), then it will be a stateful component
 
-    const clickHandler = () => {
-        setTitle('Updated!');
-        console.log(title);
-    }
+const ExpenseItem = (props) => {
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">{`$${props.amount}`}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,30 @@
 // You never need to import React module but in old
 // project they used to import.
-// import React from 'react';
+import React, { useState } from 'react';
 import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 
 const Expenses = (props) => {
+
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const onFilterDataHandler = (selectedFilterData) => {
+        console.log('This is from Expenses.js');
+        console.log(selectedFilterData);
+
+        setFilteredYear(selectedFilterData);
+    }
+
     return(
         <Card className="expenses">
+            {/* Expenses Filter is a controlled component, the selected value
+            and handling fundtion is both controlled in the parent component "Expense.js"
+            rather than "ExpenseFilter.js" 
+            If there was no controlled stuff then ExpenseFilter would be an uncontrolled
+            component. */}
+            <ExpensesFilter selected={filteredYear} onFilterData={onFilterDataHandler} />
             <ExpenseItem 
                 title={props.expenses[0].title} 
                 amount={props.expenses[0].amount} 
