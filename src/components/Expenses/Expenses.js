d020651087840f7f2cbae5c637f1ac295e725881
gingercook <vahitaras@gmail.com>
2021-11-21 23:01:36 -0500

    ExpensesList conponent added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 // You never need to import React module but in old
 // project they used to import.
 import React, { useState } from 'react';
-import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -21,70 +21,19 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
-    {/* Alternative 3 */}
+    return (
+        <li>
+            <Card className="expenses">
+                {/* Expenses Filter is a controlled component, the selected value
+                and handling fundtion is both controlled in the parent component "Expense.js"
+                rather than "ExpenseFilter.js" 
+                If there was no controlled stuff then ExpenseFilter would be an uncontrolled
+                component. */}
+                <ExpensesFilter selected={filteredYear} onFilterData={onFilterDataHandler} />
 
-    let expensesContent = <p>No Expenses Found!</p>;
-
-    if(filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map(expense => (
-            <ExpenseItem 
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-            />
-            )
-        );
-    }
-
-    {/* Alternative 3 Finishes */}
-
-    return(
-        <Card className="expenses">
-
-
-            {/* Expenses Filter is a controlled component, the selected value
-            and handling fundtion is both controlled in the parent component "Expense.js"
-            rather than "ExpenseFilter.js" 
-            If there was no controlled stuff then ExpenseFilter would be an uncontrolled
-            component. */}
-            <ExpensesFilter selected={filteredYear} onFilterData={onFilterDataHandler} />
-
-            {/* Alternative 1 */}
-            
-            {/* {filteredExpenses.length === 0 ? <p>No Expenses Found!</p> : 
-                filteredExpenses.map(expense => (
-                    <ExpenseItem 
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                    
-                    )
-                )
-            } */}
-
-            {/* Alternative 2 */}
-
-
-            {/* {filteredExpenses.length === 0 && <p>No Expenses Found!</p>}
-            {filteredExpenses.length > 0 && 
-                filteredExpenses.map(expense => (
-                    <ExpenseItem 
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                    )
-                )
-            } */}
-
-            {/* Alternative 3 */}
-
-            {expensesContent}
-        </Card>
+                <ExpensesList items={filteredExpenses} />
+            </Card>
+        </li>
     );
 }
 
