13cb40ac3200007baa629410ec497017a3aa29e3
gingercook <vahitaras@gmail.com>
2021-11-21 22:21:30 -0500

    conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -21,26 +21,69 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
-    console.log(filteredExpenses);
+    {/* Alternative 3 */}
+
+    let expensesContent = <p>No Expenses Found!</p>;
+
+    if(filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map(expense => (
+            <ExpenseItem 
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+            )
+        );
+    }
+
+    {/* Alternative 3 Finishes */}
 
     return(
         <Card className="expenses">
+
+
             {/* Expenses Filter is a controlled component, the selected value
             and handling fundtion is both controlled in the parent component "Expense.js"
             rather than "ExpenseFilter.js" 
             If there was no controlled stuff then ExpenseFilter would be an uncontrolled
             component. */}
             <ExpensesFilter selected={filteredYear} onFilterData={onFilterDataHandler} />
-            {filteredExpenses.length === 0 ? <p>No Expenses Found!</p> : 
-                filteredExpenses.map(expense => 
+
+            {/* Alternative 1 */}
+            
+            {/* {filteredExpenses.length === 0 ? <p>No Expenses Found!</p> : 
+                filteredExpenses.map(expense => (
                     <ExpenseItem 
                         key={expense.id}
                         title={expense.title}
                         amount={expense.amount}
                         date={expense.date}
                     />
+                    
+                    )
                 )
-            }
+            } */}
+
+            {/* Alternative 2 */}
+
+
+            {/* {filteredExpenses.length === 0 && <p>No Expenses Found!</p>}
+            {filteredExpenses.length > 0 && 
+                filteredExpenses.map(expense => (
+                    <ExpenseItem 
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                    )
+                )
+            } */}
+
+            {/* Alternative 3 */}
+
+            {expensesContent}
         </Card>
     );
 }
